982600a39bd3c894becfef4848d2eb75f74e55b3
Juan Fernandez <juanfernvill@gmail.com>
2025-06-08 22:12:38 +0200

    arreglo puppeteer

--- controllers/paginaController.js
+++ controllers/paginaController.js
@@ -488,7 +488,9 @@ const apuntarse  = async (req, res) => {
             }
 
             try {
-                const browser = await puppeteer.launch();
+                const browser = await puppeteer.launch({
+                    args: ['--no-sandbox', '--disable-setuid-sandbox']
+                });
                 const page = await browser.newPage();
                 await page.setContent(contenidoMail, { waitUntil: 'networkidle0' });
                 const buffer = await page.pdf({ format: 'A5', printBackground: true });
